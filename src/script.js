7b69481bab1a2671ecbc19a6b6469c66fb25817c
Isa <kmasi4@gmail.com>
2025-03-25 15:46:07 -0300

    Horário

--- src/script.js
+++ src/script.js
@@ -1,20 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
+    Saudacao();
     const botaoBuscar = document.querySelector("button");
     
     botaoBuscar.addEventListener("click", buscarClima);
 });
 
+function Saudacao() {
+    const hora = new Date().getHours();
+    let saudacao = document.getElementById("Saudacao").textContent;
+
+    if (hora >= 5 && hora < 12) {
+        saudacao = "Bom dia!";
+    } else if (hora >= 12 && hora < 18) {
+        saudacao = "Boa tarde!";
+    } else {
+        saudacao = "Boa noite!";
+    }
+
+    document.getElementById("Saudacao").textContent = saudacao;
+}
+
 async function buscarClima() {
     const apiKey = "2af363ce7663ae2ea4559b21638b2e43";
-    const inputCidade = document.getElementById("cidade");
-    const cidade = inputCidade.value.trim();
+    const inputBusca = document.getElementById("busca");
+    const busca = inputBusca.value.trim();
 
-    if (cidade === "") {
+    if (busca === "") {
         alert("Por favor, digite uma cidade válida!");
         return;
     }
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${busca}&appid=${apiKey}&units=metric&lang=pt_br`;
 
     try {
         const resposta = await fetch(url);
@@ -35,15 +51,15 @@ function exibirDados(dados) {
     const temperatura = Math.round(main.temp);
     const descricao = weather[0].description;
     
-    document.querySelector("article").innerHTML = `
+    let InfosPrincipais = document.getElementById('InfosPrincipais');
+
+    document.getElementById('Saudacao').classList.add("hidden");
+    InfosPrincipais.classList.remove("hidden");
+    InfosPrincipais.innerHTML = `
         <p class="font-medium mb-1">${name}</p>
         <p class="font-bold text-7xl mb-1">${temperatura}º</p>
         <p class="mb-1">Min: ${main.temp_min}º / Max: ${main.temp_max}º</p>
         <p class="mb-5 capitalize">${descricao}</p>
-
-        <input type="text" id="cidade" placeholder="Cidade, Bairro, Estado" 
-            class="outline-0 p-2 mb-3 rounded-xl w-full bg-white/50 text-sky-700">
-        <button class="bg-blue-500 text-white font-medium px-4 py-2 rounded-xl w-full">Buscar</button>
     `;
 
     // Re-adiciona o evento ao botão porque o HTML foi substituído
